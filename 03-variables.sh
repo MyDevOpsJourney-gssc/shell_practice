07b66183a6557fa414d6749591521f2954f3bc54
MyDevopsJourney-gssc <newdevopsjourney@gmail.com>
2026-06-26 23:33:45 +0530

    3rd shell script-variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Iran
+echo "$PERSON1: Hey, $PERSON2 we are going to blast you"
+echo "PERSON2: Yes, please. You are welcome"
+echo "$PERSON1: Hey, PERSON2 I am serious!!"
+echo "PERSON2: Hey $PERSON1, We are also serious!!!"
