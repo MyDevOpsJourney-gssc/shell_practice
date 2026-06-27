f1b3b1908c53afdcf63cb7d61665c640f43c6e01
MyDevopsJourney-gssc <newdevopsjourney@gmail.com>
2026-06-27 22:25:54 +0530

    num and string adding

--- 08-sum.sh
+++ 08-sum.sh
@@ -6,3 +6,10 @@ Val1=$1
 Val2=$2
 sum=$(($Val1+$Val2))
 echo "the sum of values $Val1 and $Val2 is $sum"
+
+
+MOVIES=("Inception" "Interstellar" "The Dark Knight" "Tenet")
+echo "Movies in the array are ${MOVIES[@]}"
+echo "The first movie name is ${MOVIES[0]}"
+echo "The second movie name is ${MOVIES[1]}"
+
