4db03262bc7e302cd7755a7553b129d7bb142913
MyDevopsJourney-gssc <newdevopsjourney@gmail.com>
2026-06-27 22:28:17 +0530

    Movies Array

---
+++ moviesArray.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MOVIES=("Inception" "Interstellar" "The Dark Knight" "Tenet")
+echo "Movies in the array are ${MOVIES[@]}"
+echo "The first movie name is ${MOVIES[0]}"
+echo "The second movie name is ${MOVIES[1]}"
