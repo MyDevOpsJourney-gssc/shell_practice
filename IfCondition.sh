52988909b221e1839632da996b25718df5dd583a
MyDevopsJourney-gssc <newdevopsjourney@gmail.com>
2026-06-27 22:37:00 +0530

    if condition

--- IfCondition.sh
+++ IfCondition.sh
@@ -4,6 +4,8 @@ NUMBER1=$1
 
 if [ $NUMBER1 -gt 10 ]; then
     echo "The number is greater than 10"
+elif [ $NUMBER1 -eq 10 ]; then
+    echo "The number is equals to 10"
 else
-    echo "The number is less than or equals to 10"
+    echo "The number is less than to 10"
 fi
